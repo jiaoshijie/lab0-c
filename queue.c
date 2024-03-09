289e4680d79c5c5f3233750965c3b5b67e298f13
jiaoshijie <jiao1751959040@gmail.com>
2024-03-09 21:03:04 +0800

    Implement q_ascend and q_descend
    try to implement merge_sort but not finished, and q_merge not
    implemented

--- queue.c
+++ queue.c
@@ -4,6 +4,10 @@
 
 #include "queue.h"
 
+#define cmp(a, b)                                  \
+    (strcmp(list_entry(a, element_t, list)->value, \
+            list_entry(b, element_t, list)->value))
+
 /* Notice: sometimes, Cppcheck would find the potential NULL pointer bugs,
  * but some of them cannot occur. You can suppress them by adding the
  * following line.
@@ -249,27 +253,31 @@ void q_reverseK(struct list_head *head, int k)
     // https://leetcode.com/problems/reverse-nodes-in-k-group/
 }
 
+#if 1
+// NOTE: Quick sort in the worst situation will failed and the STACK_LEVEL also
+// is a problem
 /* Sort elements of queue in ascending/descending order */
 void q_sort(struct list_head *head, bool descend)
 {
     if (!head || list_empty(head) || list_is_singular(head))
         return;
 
-#define STACK_LEVELS 1000  // NOTE: non-recursive quick sort max stack level
-#define cmp(a, b)                                  \
-    (strcmp(list_entry(a, element_t, list)->value, \
-            list_entry(b, element_t, list)->value))
+#define STACK_LEVEL 10000  // NOTE: non-recursive quick sort max stack level
 
     int sp = 0;  // stack pointer
-    struct list_head *beg[STACK_LEVELS], *end[STACK_LEVELS], *L, *R, *pivot,
-        *temp;
+    struct list_head *beg[STACK_LEVEL], *end[STACK_LEVEL], *L, *R, *pivot,
+        *temp, *beg_flag, *end_flag;
     beg[0] = head->next;
     end[0] = head->prev;
     while (sp >= 0) {
         L = beg[sp];
         R = end[sp];
+        beg_flag = L->prev;
+        end_flag = R->next;
         if (L != R) {
             pivot = L;
+            L = L->next;
+            list_del(pivot);
             while (L != R) {
                 while (L != R && cmp(R, pivot) > 0) {
                     R = R->prev;
@@ -278,7 +286,7 @@ void q_sort(struct list_head *head, bool descend)
                     temp = R;
                     R = R->prev;
                     list_del(temp);
-                    list_add(temp, L);
+                    list_add_tail(temp, L);
                 }
                 while (L != R && cmp(L, pivot) < 0) {
                     L = L->next;
@@ -290,35 +298,140 @@ void q_sort(struct list_head *head, bool descend)
                     list_add(temp, R);
                 }
             }
-            list_del(pivot);
             if (cmp(L, pivot) > 0) {
-                list_add(pivot, L->prev);
+                list_add_tail(pivot, L);
             } else {
                 list_add(pivot, L);
             }
-            beg[sp + 1] = pivot->next;
-            end[sp + 1] = end[sp];
-            end[sp++] = pivot->prev;
+
+            // TODO: if the range only contains a single one node, consider
+            // don't push this range to the stack.
+            beg[sp + 1] = pivot->next == end_flag ? pivot : pivot->next;
+            end[sp + 1] = end_flag->prev;
+            beg[sp] = beg_flag->next;
+            end[sp++] = pivot->prev == beg_flag ? pivot : pivot->prev;
+
         } else {
             sp--;
         }
     }
 }
+#endif
+#if 0
+/* Sort elements of queue in ascending/descending order */
+void q_sort(struct list_head *head, bool descend)
+{
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;
+
+    void recursive_merge(struct list_head **beg, struct list_head **end, bool descend) {
+        if (*beg == *end) return;
+        struct list_head *p = *beg, *q = *end, *mid, *midn, *temp;
+
+        while (p != q && p->next != q) {
+            p = p->next;
+            q = q->prev;
+        }
+        mid = p;
+        midn = mid->next;
+        recursive_merge(beg, &mid, descend);
+        recursive_merge(&midn, end, descend);
+
+        if (cmp(*beg, midn) > 0) {
+            temp = midn->next;
+            list_del(midn);
+            list_add_tail(midn, *beg);
+            *beg = midn;
+            midn = temp;
+        }
+        p = (*beg)->next;
+        q = midn;
+
+        while (p != mid->next && q != (*end)->next) {
+            if (cmp(p, q) > 0) {
+                temp = q;
+                q = q->next;
+                list_del(q);
+                list_add_tail(q, p);
+            } else {
+                p = p->next;
+            }
+        }
+
+        // TODO: this situation maybe just do nothing
+        // if (q != (*end)->next) {
+        // }
+
+        if (p != mid->next) {
+            *end = mid;
+        }
+
+    }
+
+    struct list_head *b = head->next, *e = head->prev;
+    recursive_merge(&b, &e, descend);
+}
+#endif
 
 /* Remove every node which has a node with a strictly less value anywhere to
  * the right side of it */
 int q_ascend(struct list_head *head)
 {
+    if (!head || list_empty(head))
+        return 0;
+    if (list_is_singular(head))
+        return 1;
+
+    int size = 1;
+    element_t *min = list_entry(head->prev, element_t, list), *ele;
+    struct list_head *p = head->prev->prev, *temp;
+    while (p != head) {
+        ele = list_entry(p, element_t, list);
+        if (strcmp(min->value, ele->value) < 0) {
+            temp = p;
+            p = p->prev;
+            list_del(temp);
+            free(ele->value);
+            free(ele);
+        } else {
+            size++;
+            min = ele;
+            p = p->prev;
+        }
+    }
+
     // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    return size;
 }
 
 /* Remove every node which has a node with a strictly greater value anywhere to
  * the right side of it */
 int q_descend(struct list_head *head)
 {
+    if (!head || list_empty(head))
+        return 0;
+    if (list_is_singular(head))
+        return 1;
+
+    int size = 1;
+    element_t *max = list_entry(head->prev, element_t, list), *ele;
+    struct list_head *p = head->prev->prev, *temp;
+    while (p != head) {
+        ele = list_entry(p, element_t, list);
+        if (strcmp(max->value, ele->value) > 0) {
+            temp = p;
+            p = p->prev;
+            list_del(temp);
+            free(ele->value);
+            free(ele);
+        } else {
+            size++;
+            max = ele;
+            p = p->prev;
+        }
+    }
     // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    return size;
 }
 
 /* Merge all the queues into one sorted queue, which is in ascending/descending
